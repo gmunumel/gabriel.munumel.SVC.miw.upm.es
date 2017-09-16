4681f07d69c188134ea3af61b059c335f5de84de
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-09-17 01:25:26 +0200

    Created Class C52Test

---
+++ src/test/java/gabriel/munumel/SVC/miw/upm/es/C52Test.java
@@ -0,0 +1,20 @@
+package gabriel.munumel.SVC.miw.upm.es;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C52Test {
+	private C52 c52;
+    
+    @Before
+    public void before() {
+        c52 = new C52();
+    }
+
+    @Test
+    public void testMA() {
+        assertEquals("mA", c52.mA());
+    }
+}
